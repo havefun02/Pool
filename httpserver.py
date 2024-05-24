88302ab0a4ea7055574c4387e31238e770d89a33
lapphan <lapphan@lapphan.myguest.virtualbox.org>
2024-05-24 13:12:46 +0700

    update pool api

--- httpserver.py
+++ httpserver.py
@@ -10,20 +10,23 @@ database=Database()
 
 # API endpoint to get the data
 def MinertoJson(obj):
-    t=({"username":obj.username,"target":str(obj.target),"hashrate":obj.hashrate})
+    t={"username":obj.username,"target":str(obj.target),"hashrate":obj.hashrate}
     return t
 def toJsonShareData(data):
     target=block_bits2target(data.target_network)
-    return {"id":data.id,"share":data.difficulty,"target":str(target.hex()),"datetime":data.datetime}
-@app.route('/api/data/<int:user_id>', methods=['GET'])
+    return {"id":data.share_id,"share":data.difficulty,"target":str(target.hex()),"datetime":data.datetime}
+@app.route('/users/<int:user_id>', methods=['GET'])
 def get_data(user_id):
-    user_data=database.find_one(Miner,id=user_id)
-    if user_data:    
-        data_shares=database.find_all(ShareRecord,id_user=user_id)
-        data_list = [toJsonShareData(record) for record in data_shares]
+    user_data=database.find_one(Miner,user_id=user_id)
+    # if user_data:    
+    #     data_shares=database.find_all(ShareRecord,user_id=user_id)
+    #     data_list = [toJsonShareData(record) for record in data_shares]
+    if not user_data:
+        return jsonify({"data":{} })
+        
 
-    return jsonify({"data":data_list })
-@app.route('/api/data', methods=['GET'])
+    return jsonify({"data":MinertoJson(user_data) })
+@app.route('/users/data', methods=['GET'])
 def get_pool_data():
     response=[]
     # user_data=database.custom_query("select * from miners,share_record where miners.id=share_record.id_user")
@@ -35,10 +38,10 @@ def get_pool_data():
     for user in users:
         # json_user.append(MinertoJson(user))
         if user.hashrate is not None:
-            tt_hashrate+=float(str(user.hashrate))
+            tt_hashrate+=int(str(user.hashrate))
     rewards=database.find_all(Reward)
     for reward in rewards:
-        t=rpc_gettxn(reward.id,reward.block)
+        t=rpc_gettxn(reward.reward_id,reward.block)
         total+=t["vout"][0]["value"]
     pool_data=({"title":"Pool Information",
     "miners": len(users),
